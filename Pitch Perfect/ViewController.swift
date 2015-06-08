4bc670dfc471d2c3ef68dd56647e59dd8d3b319d
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-06-08 20:36:48 -0300

    bug fix

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -10,25 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var recordinInProgress: NSLayoutConstraint!
+    @IBOutlet weak var recordingInProgress: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    // IB stands for Interface Builder
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func recordAudio(sender: UIButton) {
-        // TODO Show text "recording in progress"
-        // TODO Record the user's voice
-       
-        println("in recordAudio")
+        recordingInProgress.hidden = false
     }
-
 }
 
