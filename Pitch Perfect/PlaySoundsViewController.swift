b72dbcfa7b8b31d2ea420130b804a392a32f3821
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-06-11 20:30:13 -0300

    sounds effects

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
+            var fileUrl = NSURL.fileURLWithPath(filePath)
+            audioPlayer = AVAudioPlayer(contentsOfURL: fileUrl, error: nil)
+            audioPlayer.enableRate = true
+        }else{
+            println("the filePath is empty")
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -22,10 +32,22 @@ class PlaySoundsViewController: UIViewController {
     }
     
     @IBAction func playSlowAudio(sender: UIButton) {
-        
         //play audio slowly here...
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
     }
 
+    @IBAction func playFastAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 1.5
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    @IBAction func stopAudio(sender: UIButton) {
+        audioPlayer.stop()
+    }
     /*
     // MARK: - Navigation
 
