ebbff3a6ddcc361c57f005f2906fc882409c34e4
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-06-15 11:00:29 -0300

    added an audio file path using time stamp

---
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -0,0 +1,74 @@
+//
+//  RecordSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Guilherme Paiva on 03/06/15.
+//  Copyright (c) 2015 Guilherme Paiva. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController {
+
+    @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    var audioRecorder:AVAudioRecorder!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // hide the stop button
+        //stopButton.hidden = true
+        recordButton.enabled = true
+    }
+
+    @IBAction func recordAudio(sender: UIButton) {
+        //stopButton.hidden = false
+        recordingInProgress.hidden = false
+        recordButton.enabled = false
+        
+        //record the user's voice
+        var dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
+        var currentDateTime = NSDate()
+        var formatter = NSDateFormatter()
+        formatter.dateFormat = "ddMMyyyy-HHmmss"
+        var recordingName = formatter.stringFromDate(currentDateTime) + ".wav"
+        var pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        println(filePath)
+        
+        //setup the audio session
+        var session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+        //initialize and prepare the recorder
+        audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+        
+    }
+    
+    @IBAction func stopAudio(sender: UIButton) {
+        recordButton.enabled = true
+        recordingInProgress.hidden = true
+        //TODO: stop recording the user's voice
+        audioRecorder.stop()
+        var audioSession = AVAudioSession.sharedInstance()
+        audioSession.setActive(false, error: nil)
+    }
+   
+    
+}
+
